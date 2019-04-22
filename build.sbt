33e3891083fdae78f11a6b2b15ce2f54797d2bc7
Emmanuel Robert Ssebaggala <emmanuel.ssebaggala@bodastage.com>
2019-04-22 13:38:18 +0300

    Parse Nokia PM XML files

---
+++ build.sbt
@@ -0,0 +1,11 @@
+name := "boda-nokiapmdataparser"
+
+version := "0.1"
+
+scalaVersion := "2.12.8"
+
+libraryDependencies += "org.scalactic" %% "scalactic" % "3.0.5"
+
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.5" % "test"
+
+libraryDependencies += "org.scala-lang.modules" %% "scala-xml" % "1.0.6"
--- src/main/scala/com/bodastage/nokia/BodaNokiaPMDataParser.scala
+++ src/main/scala/com/bodastage/nokia/BodaNokiaPMDataParser.scala
@@ -1,7 +1,86 @@
-object BodaNokiaPMDataParser{
+package com.bodastage.nokia
 
+import scala.io.Source
+import scala.xml.pull._
+import scala.collection.mutable.ArrayBuffer
+import java.io.File
+import java.io.FileOutputStream
+import scala.xml.XML
+
+object BodaNokiaPMDataParser{
   def main(args: Array[String]): Unit = {
-    println("Hello world!");
+
+    var startTime: String = "";
+    var interval: String = "";
+    var moBaseId: String = "";
+    var moLocalMoId: String= "";
+    var neType:String = "";
+    var measType: String= "";
+    var counterId: String= "";
+    var counterValue: String = ""
+    var fileName: String = ""
+
+    val xmlFile = args(0)
+    val outputDirectory = new File(args(1))
+    fileName = xmlFile
+
+    val xml = new XMLEventReader(Source.fromFile(xmlFile))
+    var buf = ArrayBuffer[String]()
+
+    println("FILENAME,startTime,interval,baseId,localMoId,ne,measurementType,counterId,counterValue")
+
+    for(event <- xml){
+      event match {
+        case EvElemStart(_, tag, attrs, _) => {
+          buf.clear
+
+          if(tag == "PMSetup"){
+            for(m <- attrs){
+              if(m.key == "startTime") startTime = m.value.toString()
+              if(m.key == "interval") interval = m.value.toString()
+            }
+          }
+
+          if( tag.matches("NE-.*")){
+            val pattern = "NE-(.*)".r
+            val pattern(ne) = tag
+            neType = ne
+
+            for(m <- attrs){
+              if(m.key == "measurementType") measType = m.value.toString()
+            }
+          }
+
+
+        }
+        case EvText(t) => {
+          buf += (t)
+        }
+
+        case EvElemEnd(_, tag) => {
+          if(tag == "baseId"){
+            moBaseId = buf.mkString
+          }
+
+          if(tag == "localMoid"){
+            moLocalMoId = buf.mkString
+          }
+
+          if( tag.matches("""M\d+C\d+""")){
+              counterId = tag
+              counterValue = buf.mkString
+
+              println(s"$fileName,$startTime,$interval,$moBaseId,$moLocalMoId,$neType,$measType,$counterId,$counterValue")
+          }
+
+          buf.clear
+        }
+
+        case _ =>
+      }
+    }
+
   }
+
 
 }
